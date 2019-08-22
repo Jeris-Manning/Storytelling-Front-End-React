50360470a38c3f4d8ea46e964d76e0aa54125871
Logan Karnes <logan.karnes98@gmail.com>
2019-08-21 21:14:43 -0700

    fixed merge

---
+++ src/components/StoryCard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function StoryCard(props){
+    return (
+        <div>
+            storyCard
+            <button>Donate</button>
+        </div>
+    )
+    }
